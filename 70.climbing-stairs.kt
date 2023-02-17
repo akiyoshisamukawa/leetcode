406c15e2b4f4f019acd245b4d03c9cc3b47dc8da
akiyoshisamukawa <arcmoon.rey619@gmail.com>
2023-02-17 14:54:23 +0900

    70.climbing-stairs.kt

---
+++ 70.climbing-stairs.kt
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=70 lang=kotlin
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+class Solution {
+    fun climbStairs(n: Int): Int {
+        when (n) {
+            0,1 -> return 1
+            2 -> return 2
+            else -> {
+                var i = 1
+                var j = 2
+                (3 .. n).forEach {
+                    val k = j
+                    j = i + j
+                    i = k
+                }
+                return j
+            }
+        }
+    }
+}
+// @lc code=end
+
